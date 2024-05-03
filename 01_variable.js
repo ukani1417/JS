2a826cff76362246c9aaf9915029b6cb638acb0e
fc20412 <dhruv.ukani@freecharge.com>
2024-05-03 20:20:01 +0530

    variable is completed

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+const accountId = "fc20412"
+let accountEmail = "dhruv.ukani@freecharge.com"
+var accountPass = "@Dbu8849"
+accountCity = "Valukad"
+let accountState;
+// accountId = "fc20411" can not work
+
+accountEmail = "ukanid279@gmail.com"
+accountPass = "@Dboy9510"
+
+/*
+Should only use const and let, because var work with global space.
+*/
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
